55e7d7fc5b5e2a0e3ddac8db267d477538a9c869
CIPDRepo <97507542+CIPDRepo@users.noreply.github.com>
2022-01-11 09:36:17 +0000

    Update InstallRebootAgent.ps1

--- InstallRebootAgent.ps1
+++ InstallRebootAgent.ps1
@@ -1,6 +1,5 @@
 $vms = Get-AzVM | Select-Object ResourceGroupName,Name,Location
-$SourceScriptURI = 'https://github.com/CIPDRepo/VLZ/blob/45aca86dc22564d29ab35404c4ba1780797e5e14/RebootAgent.ps1'
-
+$SourceScriptURI = 'https://raw.githubusercontent.com/CIPDRepo/VLZ/main/RebootAgent.ps1'
 
 ForEach ($vm in $vms) {
     Set-AzVMRunCommand `
