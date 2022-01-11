1427d2a139a5c45d9ae4ecb9cdceb2b42765f383
CIPDRepo <97507542+CIPDRepo@users.noreply.github.com>
2022-01-11 07:58:22 +0000

    Uploaded the ps1 and msi

---
+++ RebootAgent.ps1
@@ -0,0 +1,12 @@
+$drive = 'C:\Packages'
+$appName = 'fs-windows-agent-2.9.0'
+New-Item -Path $drive -Name $appName -ItemType Directory -ErrorAction SilentlyContinue
+$LocalPath = $drive + '\' + $appName
+Set-Location -Path $LocalPath
+$URL = 'https://fileinstall.blob.core.windows.net/fileinstall/fs-windows-agent-2.9.0.msi'
+$msi = 'fs-windows-agent-2.9.0.msi'
+$outputPath = $LocalPath + '\' + $msi
+Invoke-WebRequest -Uri $URL -OutFile $outputPath
+write-host 'Starting Install fs-windows-agent-2.9.0'
+Start-Process -FilePath msiexec.exe -Args "/package $outputPath" -Wait
+write-host 'Finished Install the of fs-windows-agent-2.9.0'
