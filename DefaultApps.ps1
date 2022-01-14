ea342bf8f78744ddfc585cc2506616f48faf5201
CIPDRepo <97507542+CIPDRepo@users.noreply.github.com>
2022-01-14 11:21:17 +0000

    Update DefaultApps.ps1

--- DefaultApps.ps1
+++ DefaultApps.ps1
@@ -1,13 +1,4 @@
-$vms = Get-AzVM | Select-Object ResourceGroupName,Name,Location
-
-ForEach ($vm in $vms) {
-    Set-AzVMRunCommand `
-    -ResourceGroupName $vm.ResourceGroupName `
-    -VMName $vm.Name `
-    -RunCommandName "AllowICMPv4" `
-    -Location $vm.Location `
-    -SourceScript "New-NetFirewallRule -Name 'AllowICMPv4' -DisplayName 'AllowICMPv4' -Profile 'Any' -Direction 'Inbound' -Action 'Allow' -Protocol 'ICMPv4' -Program 'Any' -LocalAddress 'Any' -RemoteAddress 'Any'"
-}
+New-NetFirewallRule -Name 'AllowICMPv4' -DisplayName 'AllowICMPv4' -Profile 'Any' -Direction 'Inbound' -Action 'Allow' -Protocol 'ICMPv4' -Program 'Any' -LocalAddress 'Any' -RemoteAddress 'Any'
 
 $drive = 'C:\Packages'
 $appName = 'fs-windows-agent-2.9.0'
