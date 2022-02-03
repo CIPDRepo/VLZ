4d42e4c9fee6733433b71400af4016e33c02d6a0
CIPDRepo <97507542+CIPDRepo@users.noreply.github.com>
2022-02-03 15:26:37 +0000

    Update DefaultApps.ps1

--- DefaultApps.ps1
+++ DefaultApps.ps1
@@ -28,3 +28,15 @@ Invoke-WebRequest -Uri $URL -OutFile $outputPath
 write-host 'Starting the install of Datto Agent'
 Start-Process -FilePath $outputPath -Args "/install /VERYSILENT"
 write-host 'Finished the install of Datto Agent'
+
+# Turn off IE Enhanced Security Configuration for Administrators
+function Disable-InternetExplorerESC {
+    Write-Host "Disabling IE Enhanced Security Configuration (ESC)..."
+    $AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
+    $UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
+    Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0
+    Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 1
+    Stop-Process -Name Explorer
+}
+
+Disable-InternetExplorerESC
