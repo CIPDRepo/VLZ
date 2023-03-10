fd956ea8fc9ef4dd02e4476095fe111691d1415e
KrisTurk <51917397+Kris-Turk@users.noreply.github.com>
2023-03-10 15:26:05 +1300

    Update DefaultApps.ps1

--- DefaultApps.ps1
+++ DefaultApps.ps1
@@ -36,7 +36,7 @@ function Disable-InternetExplorerESC {
     $UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
     Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0
     Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 1
-    Stop-Process -Name Explorer
+    Stop-Process -Name Explorer -Force
 }
 # Disable ESC
 Disable-InternetExplorerESC
